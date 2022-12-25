529d48098808e5e44a0cfa287c006f85ec494874
Ci3t <ranigfx@gmail.com>
2022-12-25 17:00:13 +0200

    Changes

---
+++ client/src/components/admin/AnimeUpload.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { FileUploader } from "react-drag-drop-files";
+import { AiOutlineCloudUpload } from "react-icons/ai";
+import { uploadTrailer } from "../../api/anime";
+import { useNotification } from "../../hooks/themeHook";
+
+function AnimeUpload() {
+  const [videoSelected, setVideoSelected] = useState(false);
+  const [videoUploaded, setVideoUploaded] = useState(false);
+  const [uploadProgress, setUploadProgress] = useState(0);
+  const { updateNotification } = useNotification();
+  const handleChange = async (file) => {
+    const formData = new FormData();
+    formData.append("video", file);
+
+    setVideoSelected(true)
+    const res = await uploadTrailer(formData,setUploadProgress);
+    if(!res.error){
+        setVideoUploaded(true)
+    }
+  };
+
+  const handleTypeError = (error) => {
+    updateNotification("error", error);
+  };
+
+  const getUploadProgressValue =()=>{
+    if(!videoUploaded && uploadProgress >= 100){
+        return 'Processing'
+    } 
+
+    return `Upload Progress ${uploadProgress} %`
+  }
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex items-center justify-center">
+      <div className="dark:bg-main bg-white rounded w-[45rem] h-[40rem] overflow-auto p-2">
+
+     
+
+        <UploadProgress visible={!videoUploaded && videoSelected} message={getUploadProgressValue()} width={uploadProgress}/>
+        <TrailerSelector
+          visible={!videoSelected}
+          onTypeError={handleTypeError}
+          handleChange={handleChange}
+        />
+      </div>
+    </div>
+  );
+}
+
+function TrailerSelector({ visible, handleChange, onTypeError }) {
+  if (!visible) return null;
+  return (
+    <div className="h-full flex items-center justify-center">
+      <FileUploader
+        handleChange={handleChange}
+        onTypeError={onTypeError}
+        types={["mp4", "avi"]}
+      >
+        <div className="w-48 h-48 border border-dashed dark:border-dark-subtle rounded-full flex items-center justify-center flex-col dark:text-light-subtle cursor-pointer">
+          <AiOutlineCloudUpload size={80} />
+          <p>Drop your file here!</p>
+        </div>
+      </FileUploader>
+    </div>
+  );
+}
+
+function UploadProgress ({width,message,visible}){
+
+    if(!visible) return null
+    return (
+       
+        <div className="rounded p-3 dark:bg-second  drop-shadow-lg ">
+            <div className="h-3 overflow-hidden relative bg-light-subtle dark:bg-dark-subtle">
+                <div style={{width:width + '%'}} className="h-full absolute left-0 bg-second dark:bg-white"/>
+                
+            </div>
+            <p className="font-semibold dark:text-dark-subtle text-light-subtle animate-pulse mt-1">{message}</p>
+        </div>
+
+
+    )
+}
+export default AnimeUpload;
--- client/src/components/admin/Dashboard.jsx
+++ client/src/components/admin/Dashboard.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
+import AnimeUpload from './AnimeUpload'
 
 function Dashboard() {
   return (
-    <div className='fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm flex items-center justify-center'>
-
-        <div className="dark:bg-main bg-white rounded w-[45rem] h-[40rem] overflow-auto"></div>
-
-    </div>
+    <AnimeUpload/>
   )
 }
 
