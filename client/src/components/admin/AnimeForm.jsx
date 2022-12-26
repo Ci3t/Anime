26a6acb7899c0502a828570e183776c5f5974a4a
Ci3t <ranigfx@gmail.com>
2022-12-26 13:27:00 +0200

    handling Form

--- client/src/components/admin/AnimeForm.jsx
+++ client/src/components/admin/AnimeForm.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
+import TagsInput from './TagsInput'
 
 function AnimeForm() {
+    const handleSubmit =(e)=>{
+        e.preventDefault()
+    }
   return (
     
-    <form className='flex space-x-3 '>
-       <div class="relative z-0 mb-6 w-full group">
-      <input type="email" name="floating_email" id="floating_email" class="block pt-3 px-0 w-full text-sm text-gray-900 bg-transparent border-b-2 border-t-0 border-x-0 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-      <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 pt-2 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Title</label>
-  </div>
+    <form onSubmit={handleSubmit} className='flex space-x-3 '>
+       <div className="relative z-0 mb-6  group w-[70%] space-x-3">
+
+        <div>
+
+      <input type="text" name='title'  id="title" className="block pt-3 px-0 w-full text-sm text-gray-900 bg-transparent border-b-2 border-t-0 border-x-0 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" "  />
+      <label htmlFor="title" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 pt-2 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Title</label>
+      
+  
+        </div>
+
+      
+    <div className='mx-0 space-x-1'>
+
+    
+      <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-black mt-2">Description</label>
+<textarea id="description"  className="block pt-3 px-0 w-full text-sm text-gray-900 bg-transparent border-b-2 border-t-0 border-x-0 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 resize-none peer"  placeholder="Enter Anime Story..."></textarea>
+ 
+    </div>
+        <TagsInput/>
+     </div>
         <div className="w-[30%] h-5 bg-blue-400"></div>
     </form>
   )
---
+++ client/src/components/admin/TagsInput.jsx
@@ -0,0 +1,84 @@
+import React, { useEffect, useRef, useState } from "react";
+import "./tagsInput.css";
+import {AiOutlineClose} from 'react-icons/ai'
+
+function TagsInput() {
+    const [tag ,setTag] = useState('')
+    const [tags ,setTags] = useState([])
+
+    const inputRef = useRef()
+    const tagsInput = useRef()
+
+    const handleOnChange = ({target})=>{
+        const {value} = target
+        
+        if(value !== ',') setTag(value)
+    }
+    const removeTag = (removeTag)=>{
+        
+        const newTags = tags.filter((tag)=>tag !==removeTag)
+        setTags([...newTags])
+    }
+
+    const handleKeyDown = ({key})=>{
+        
+        if(key === ',' || key ==='Enter'){
+            if(!tag) return ;
+
+            if(tags.includes(tag)) return setTag('');
+
+            setTags([...tags, tag]);
+            setTag('')
+        }
+
+        if(key === 'Backspace' && !tag && tags.length){
+            const newTags = tags.filter((_,index)=>index !==tags.length -1)
+            setTags([...newTags])
+        }
+
+    }
+
+    const handleOnFocus =()=>{
+        tagsInput.current.classList.remove('TagsInputContainerBlurColor')
+        tagsInput.current.classList.add('TagsInputContainerFocusColor')
+    }
+    const handleOnBlur =()=>{
+        tagsInput.current.classList.add('TagsInputContainerBlurColor')
+        tagsInput.current.classList.remove('TagsInputContainerFocusColor')
+    }
+
+    useEffect(()=>{
+        inputRef.current.scrollIntoView()
+    },[tag])
+
+    const Tag = ({children,onClick}) =>{
+       return (
+        <span className=" bg-gray-700 text-white text-sm whitespace-nowrap ">{children}
+        <button type="button" onClick={onClick}>
+            <AiOutlineClose size={12}/>
+        </button>
+        </span>
+       )
+    }
+  return (
+
+    <div className="mt-3">
+        Tags
+      <div 
+      ref={tagsInput}
+      
+      onKeyDown={handleKeyDown} className="TagsInputContainer TagsInputContainerBlurColor flex items-center space-x-2 px-2  overflow-x-auto custom-scroll-bar transition">
+       {tags.map(t=> <Tag onClick={()=>removeTag(t)} key={t}>{t}</Tag>)}
+        <input ref={inputRef} type="text" className="TagsInputField outline-none " placeholder="Tag 4, Tag 5"
+        value={tag}
+        onChange ={handleOnChange}
+        onFocus={handleOnFocus}
+      onBlur={handleOnBlur}
+         />
+        
+      </div>
+    </div>
+  );
+}
+
+export default TagsInput;
