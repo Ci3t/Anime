358918428f6ee8cf70427ed425ce1e510a8664ed
Ci3t <ranigfx@gmail.com>
2023-01-04 13:58:57 +0200

    could be final v2

--- app.js
+++ app.js
@@ -25,13 +25,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
-app.use('/',indexRoute)
+// app.use('/',indexRoute)
 
 
 const publicPath = path.join(__dirname, 'build');
 app.use(express.static(publicPath));
 
-app.use('/api', indexRoute);
+app.use('/', indexRoute);
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
