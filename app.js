5add07e0e2c663eae9c9e1c8ad05ff159d754422
Ci3t <ranigfx@gmail.com>
2023-01-04 13:08:13 +0200

    could be final

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ import express from 'express'
 import './mongoose/mongoose.connect.js'
 import * as expressAsync from "express-async-errors"
 import { indexRoute } from './routes/index.route.js';
-
+import path from 'path';
 import cors from 'cors'
+import * as url from 'url';
 // import { createRequire } from "module";
 // const require = createRequire(import.meta.url);
 //  require("express-async-errors");
@@ -14,6 +15,9 @@ import { handleNotFound } from './utils/helper.js';
 
 
 dotenv.config()
+
+const __dirname = url.fileURLToPath(new URL('./', import.meta.url));
+
 const app = express()
 const PORT = process.env.PORT || 5000;
 
@@ -23,6 +27,15 @@ app.use(cors())
 app.use(express.json())
 app.use('/',indexRoute)
 
+
+const publicPath = path.join(__dirname, 'build');
+app.use(express.static(publicPath));
+
+app.use('/api', indexRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
 
 app.get('/',(req,res)=>{
     res.send('HomePage')
