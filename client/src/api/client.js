4f2d45f9a4e21466cfb8e8d67d2fd8803460c349
Ci3t <ranigfx@gmail.com>
2023-01-04 15:08:31 +0200

    render

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,8 +1,12 @@
 import axios from "axios";
-let baseURL = 'http://localhost:5000/api'
-if(process.env.NODE_ENV === 'production' ){
-    baseURL = '/api'
+
+let baseURL = 'http://localhost:5000/api';
+
+if (process.env.NODE_ENV === 'production') {
+    baseURL = 'api';
 }
+
+
 const client = axios.create({baseURL})
 
 export default client
